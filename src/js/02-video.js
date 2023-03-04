6fb28d3c49ea621ed4b83eee338ad7009046ac6b
Anna Shalar <113612033+latikaann@users.noreply.github.com>
2023-03-04 23:57:08 +0200

    hw ready

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,34 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+const STORAGE_KEY = 'videoplayer-current-time';
+
+player.on(
+  'timeupdate',
+  throttle(e => {
+    localStorage.setItem(STORAGE_KEY, e.seconds);
+  }, 1000)
+);
+
+const playedTime = localStorage.getItem(STORAGE_KEY);
+console.log(playedTime);
+
+player
+  .setCurrentTime(playedTime)
+  .then(function (seconds) {
+    // seconds = the actual time that the player seeked to
+  })
+  .catch(function (error) {
+    switch (error.name) {
+      case 'RangeError':
+        // the time was less than 0 or greater than the video’s duration
+        break;
+
+      default:
+        // some other error occurred
+        break;
+    }
+  });
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,43 @@
+import throttle from 'lodash.throttle';
 
+const refs = {
+  form: document.querySelector('.feedback-form'),
+  email: document.querySelector('input[name="email"]'),
+  textarea: document.querySelector('textarea[name="message"]'),
+};
+
+const { form, email, textarea } = refs;
+
+const STORAGE_KEY = 'feedback-form-state';
+
+let formData = {};
+
+form.addEventListener('submit', onFormSubmit);
+form.addEventListener('input', onFormTextContent);
+
+populateFormTextContent();
+
+function onFormSubmit(e) {
+  e.preventDefault();
+  e.currentTarget.reset();
+  localStorage.removeItem(STORAGE_KEY);
+}
+
+function onFormTextContent(e) {
+  formData[e.target.name] = e.target.value;
+
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+}
+
+function populateFormTextContent() {
+  const savedInfo = localStorage.getItem(STORAGE_KEY);
+
+  if (savedInfo) {
+    console.log(savedInfo);
+    formData = JSON.parse(savedInfo);
+
+    for (let key in formData) {
+      form.elements[key].value = formData[key];
+    }
+  }
+}
